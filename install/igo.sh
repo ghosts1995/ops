942e3f7ba20272a0c05af9417229117a06edeace
andy <ghosts.jo1995@gmail.com>
2020-01-13 16:27:03 +0800

    fix bug

--- install/igo.sh
+++ install/igo.sh
@@ -7,11 +7,9 @@ tar zxvf go1.13.6.linux-amd64.tar.gz -C /usr/local
 mkdir -p /root/.go
 
 #input evn info
-echo "export GO111MODULE=on" >> /etc/profile
-echo "export GOROOT=/usr/local/go" >> /etc/profile
-echo "export GOPATH=/root/.go"  >> /etc/profile
-echo "export PATH=$PATH:$GOROOT/bin:$GOPATH/bin" >> /etc/profile
+echo "export GO111MODULE=auto" >> /etc/bashrc
+echo "export GOROOT=/usr/local/go" >> /etc/bashrc
+echo "export GOPATH=/root/.go"  >> /etc/bashrc
+echo "export PATH=$PATH:$GOROOT/bin:$GOPATH/bin" >> /etc/bashrc
 
 #cat go env 
-
-go env
