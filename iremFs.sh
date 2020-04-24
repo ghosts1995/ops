ac653b04dc2a1038b6ed47e128e3d627cad78115
andy <ghosts.jo1995@gmail.com>
2020-04-24 16:46:46 +0800

    add

--- iremFs.sh
+++ iremFs.sh
@@ -10,3 +10,5 @@ screen -dmS $screen_name
 cmd=$"curl -s https://raw.githubusercontent.com/ghosts1995/ops/master/install/iyfs1-10.sh | bash";
 screen -x -S $screen_name -p 0 -X stuff "$cmd"
 screen -x -S $screen_name -p 0 -X stuff $'\n'
+
+echo "done"
