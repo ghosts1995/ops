e218a7c17157f3791d9fe35399b4bf80a4249ac0
andy <ghosts.jo1995@gmail.com>
2020-04-27 11:54:28 +0800

    fix bug

--- iremFs.sh
+++ iremFs.sh
@@ -9,18 +9,21 @@ echo $serverId > /var/log/voip.log
 serverAddr=$2
 echo $serverAddr > /var/log/voipAddr.log
 
-file="/usr/bin/screen"
+yum -y insyall screen 
 
-if [ ! -f $file ]; then
-  yum -y insyall screen 
-fi
+#file="/usr/bin/screen"
+
+#if [ ! -f $file ]; then
+#  yum -y insyall screen 
+#fi
 
 cd /usr/local/src
 
 screen_name=$"log"
 screen -dmS $screen_name
 
-cmd=$"curl -s https://raw.githubusercontent.com/ghosts1995/ops/master/install/iyfs1-10.sh | bash";
+cmd=$"curl -s https://raw.githubusercontent.com/ghosts1995/ops/master/install/iyfs1-10.sh | bash "
+
 screen -x -S $screen_name -p 0 -X stuff "$cmd"
 screen -x -S $screen_name -p 0 -X stuff $'\n'
 
